bbe02e98deb84a1bcac82c9a172d30f2e28b6e13
Julio Punkito <julesnoir@hotmail.fr>
2023-06-01 22:43:31 +0200

    feat(client): :sparkles: register can take image and preview it for profile pic

--- client/src/pages/RegisterPage/RegisterPage.jsx
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,99 +1,72 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { Form, Button, Spinner, Image } from "react-bootstrap";
+import { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Form, Button, Spinner, Image } from 'react-bootstrap'
 
-import { AuthState } from "../../context/AuthProvider";
-import { Notify } from "../../utils";
-import IMAGES from "../../assets";
+import { AuthState } from '../../context/AuthProvider'
+import { Notify } from '../../utils'
+import IMAGES from '../../assets'
 
 const RegisterPage = () => {
   const [credentials, setCredentials] = useState({
-    name: "",
-    password: "",
-    confirmPassword: "",
-    profilePic: "",
-  });
-  const [isLoading, setIsLoading] = useState(false);
-  const [imagePreview, setimagePreview] = useState(IMAGES.user); // Default image for preview
+    name: '',
+    password: '',
+    confirmPassword: '',
+    profilePic: '',
+  })
+  const [isLoading, setIsLoading] = useState(false)
+  const [imagePreview, setimagePreview] = useState(IMAGES.user) // Default image for preview
 
-  const navigate = useNavigate();
-  const { setAuth } = AuthState();
+  const navigate = useNavigate()
+  const { setAuth } = AuthState()
 
   const handleCredentials = (e) => {
-    setCredentials({ ...credentials, [e.target.name]: e.target.value });
-  };
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
 
   const handleProfilePic = async (e) => {
-    setIsLoading(true);
-    const profilePic = e.target.files[0]; // Get the file
-
+    setIsLoading(true)
+    const profilePic = e.target.files[0] // Get the file
+    const imageUrl = URL.createObjectURL(profilePic)
+    console.log(imageUrl)
     // Check file type
     if (
-      profilePic.type !== "image/jpeg" &&
-      profilePic.type !== "image/jpg" &&
-      profilePic.type !== "image/png"
+      profilePic.type !== 'image/jpeg' &&
+      profilePic.type !== 'image/jpg' &&
+      profilePic.type !== 'image/png'
     ) {
-      e.target.value = null; // Clear upload field
-      setimagePreview(IMAGES.user);
-      setIsLoading(false);
-      return Notify("Only .jpeg, .jpg and .png supported", "warn");
+      e.target.value = null // Clear upload field
+      setimagePreview(IMAGES.user)
+      setIsLoading(false)
+      return Notify('Only .jpeg, .jpg and .png supported', 'warn')
     }
 
     // Check file size
     if (profilePic.size > 1 * 1024 * 1024) {
-      e.target.value = null; // Clear upload field
-      setimagePreview(IMAGES.user);
-      setIsLoading(false);
-      return Notify("Please upload image under 1 MB", "warn");
+      e.target.value = null // Clear upload field
+      setimagePreview(IMAGES.user)
+      setIsLoading(false)
+      return Notify('Please upload image under 1 MB', 'warn')
     }
-
-    // Save the image in FormData object
-    const formData = new FormData();
-    formData.append("file", profilePic); // Contains the file
-    formData.append(
-      "upload_preset",
-      `${process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET}`
-    ); // Upload preset in Cloudinary
-    formData.append(
-      "cloud_name",
-      `${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}`
-    ); // Cloud name in Cloudinary
 
     try {
-      // Upload image to cloudinary if user selected an image
-      const cloudinaryResponse = await fetch(
-        `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/image/upload`,
-        {
-          method: "POST",
-          body: formData,
-        }
-      );
-      const cloudinaryData = await cloudinaryResponse.json();
-
-      // If profile pic is uploaded, set the image URL for registration
       setCredentials({
         ...credentials,
-        profilePic: cloudinaryData.secure_url.toString(),
-      });
-
-      // Image preview logic
-      const reader = new FileReader();
-      reader.readAsDataURL(profilePic);
-      reader.onload = () => setimagePreview(reader.result);
-
-      setIsLoading(false);
-      return Notify("Profile pictute uploaded", "success");
+        profilePic: imageUrl, // Mettre à jour avec l'URL de l'image
+      })
+      setimagePreview(imageUrl)
+      setIsLoading(false)
+      return Notify('Profile picture uploaded', 'success')
     } catch (error) {
-      e.target.value = null; // Clear upload field
-      setimagePreview(IMAGES.user);
-      setIsLoading(false);
-      return Notify("Internal server error", "error");
+      e.target.value = null // Clear upload field
+      setimagePreview(IMAGES.user)
+      setIsLoading(false)
+      return Notify('Internal server error', 'error')
     }
-  };
+  }
 
   const registerHandler = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
+    e.preventDefault()
+    setIsLoading(true)
 
     // If any field is missing
     if (
@@ -101,52 +74,52 @@ const RegisterPage = () => {
       !credentials.password ||
       !credentials.confirmPassword
     ) {
-      setIsLoading(false);
-      return Notify("Please Fill all the Feilds", "warn");
+      setIsLoading(false)
+      return Notify('Please Fill all the Feilds', 'warn')
     }
 
     // If password and confirm password doesn't match
     if (credentials.password !== credentials.confirmPassword) {
-      setIsLoading(false);
-      return Notify("Passwords Do Not Match", "warn");
+      setIsLoading(false)
+      return Notify('Passwords Do Not Match', 'warn')
     }
 
     // If password is less than 8 characters
     if (credentials.password.length < 8) {
-      setIsLoading(false);
-      return Notify("Password must be at least 8 characters", "warn");
+      setIsLoading(false)
+      return Notify('Password must be at least 8 characters', 'warn')
     }
 
     try {
       // Register user
-      const response = await fetch("/api/auth/register", {
-        method: "POST",
+      const response = await fetch('/api/auth/register', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           name: credentials.name,
           password: credentials.password,
           profilePic: credentials.profilePic,
         }),
-      });
-      const data = await response.json();
+      })
+      const data = await response.json()
 
       if (data.success) {
-        localStorage.setItem("auth", JSON.stringify(data)); // Save auth details in local storage
-        setAuth(data);
-        setIsLoading(false);
-        navigate("/"); // Go to home page
-        return Notify("Your account has been successfully created", "success");
+        localStorage.setItem('auth', JSON.stringify(data)) // Save auth details in local storage
+        setAuth(data)
+        setIsLoading(false)
+        navigate('/') // Go to home page
+        return Notify('Your account has been successfully created', 'success')
       } else {
-        setIsLoading(false);
-        return Notify(data.error, "error");
+        setIsLoading(false)
+        return Notify(data.error, 'error')
       }
     } catch (error) {
-      setIsLoading(false);
-      return Notify("Internal server error", "error");
+      setIsLoading(false)
+      return Notify('Internal server error', 'error')
     }
-  };
+  }
 
   return (
     <Form className="auth__form" onSubmit={registerHandler}>
@@ -158,6 +131,7 @@ const RegisterPage = () => {
           src={imagePreview}
           alt="Profile image"
           draggable="false"
+          style={{objectFit:"cover"}}
           roundedCircle
         />
       </Form.Group>
@@ -220,7 +194,7 @@ const RegisterPage = () => {
         {isLoading ? (
           <Spinner animation="border" role="status" size="sm" />
         ) : (
-          "Create Account"
+          'Create Account'
         )}
       </Button>
 
@@ -233,7 +207,7 @@ const RegisterPage = () => {
         </span>
       </Form.Group>
     </Form>
-  );
-};
+  )
+}
 
-export default RegisterPage;
+export default RegisterPage
