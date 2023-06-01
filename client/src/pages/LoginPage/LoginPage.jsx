be74ae8f688b6943cded9f3f1ac2865952a2d681
Julio Punkito <julesnoir@hotmail.fr>
2023-06-02 00:10:36 +0200

    style(client): :lipstick: made the best login page ever

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,66 +1,79 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { Form, Button, Spinner } from "react-bootstrap";
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Form, Button, Spinner } from 'react-bootstrap'
 
-import { AuthState } from "../../context/AuthProvider";
-import { Notify } from "../../utils";
+import { AuthState } from '../../context/AuthProvider'
+import { Notify } from '../../utils'
+import IMAGES from '../../assets'
 
 const LoginPage = () => {
   const [credentials, setCredentials] = useState({
-    name: "",
-    password: "",
-  });
-  const [isLoading, setIsLoading] = useState(false);
+    name: '',
+    password: '',
+  })
+  const [isLoading, setIsLoading] = useState(false)
 
-  const navigate = useNavigate();
-  const { setAuth } = AuthState();
+  const navigate = useNavigate()
+  const { setAuth } = AuthState()
 
   const handleCredentials = (e) => {
-    setCredentials({ ...credentials, [e.target.name]: e.target.value });
-  };
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
 
   const loginHandler = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
+    e.preventDefault()
+    setIsLoading(true)
 
     // If any field is missing
     if (!credentials.name || !credentials.password) {
-      setIsLoading(false);
-      return Notify("Please Fill all the Feilds", "warn");
+      setIsLoading(false)
+      return Notify('Please Fill all the Feilds', 'warn')
     }
 
     try {
-      const response = await fetch("/api/auth/login", {
-        method: "POST",
+      const response = await fetch('/api/auth/login', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           name: credentials.name,
           password: credentials.password,
         }),
-      });
-      const data = await response.json();
+      })
+      const data = await response.json()
 
       if (data.success) {
-        localStorage.setItem("auth", JSON.stringify(data)); // Save auth details in local storage
-        setAuth(data);
-        setIsLoading(false);
-        navigate("/"); // Go to home page
-        return Notify("You are successfully logged in", "success");
+        localStorage.setItem('auth', JSON.stringify(data)) // Save auth details in local storage
+        setAuth(data)
+        setIsLoading(false)
+        navigate('/') // Go to home page
+        return Notify('You are successfully logged in', 'success')
       } else {
-        setIsLoading(false);
-        return Notify(data.error, "warn");
+        setIsLoading(false)
+        return Notify(data.error, 'warn')
       }
     } catch (error) {
-      setIsLoading(false);
-      return Notify("Internal server error", "error");
+      setIsLoading(false)
+      return Notify('Internal server error', 'error')
     }
-  };
+  }
 
   return (
     <Form className="auth__form" onSubmit={loginHandler}>
-      <h3 className="text-center mb-5">Login to Your Account</h3>
+      <div className='d-flex flex-column justify-content-center align-items-center'>
+        <h3 className="text-center mb-5">
+          Login to <br /> Your Julio Shack Account
+        </h3>
+        <img
+          alt="Julio Shack"
+          src={IMAGES.julio}
+          width="150"
+          height="150"
+          className="d-inline-block align-top"
+        />
+        <h2>The best image app<br/> for shitpost purpose</h2>
+      </div>
       <Form.Group className="mb-3" controlId="name">
         <Form.Label>Username</Form.Label>
         <Form.Control
@@ -95,7 +108,7 @@ const LoginPage = () => {
         {isLoading ? (
           <Spinner animation="border" role="status" size="sm" />
         ) : (
-          "Continue"
+          'Continue'
         )}
       </Button>
 
@@ -108,7 +121,7 @@ const LoginPage = () => {
         </span>
       </Form.Group>
     </Form>
-  );
-};
+  )
+}
 
-export default LoginPage;
+export default LoginPage
