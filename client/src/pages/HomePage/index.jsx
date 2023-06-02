db11447536b36db06766f9560c0c4aaf9b5413b2
Julio Punkito <julesnoir@hotmail.fr>
2023-06-02 18:28:20 +0200

    style(client): :lipstick: updated homepage style

--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -3,7 +3,9 @@ import Wrapper from '../../components/Wrapper'
 import { getPublicImages } from '../../actions/imageAction'
 import ImageGallery from '../../components/ImageGallery'
 import HomeText from '../../components/HomeText'
-
+import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
+import './HomePage.css'
 const HomePage = () => {
   const [userImages, setUserImages] = useState([])
   const [refresh, setRefresh] = useState(false)
@@ -21,8 +23,12 @@ const HomePage = () => {
 
   return (
     <Wrapper>
-      <HomeText />
-
+      <div id="hometextblock">
+        <HomeText />
+        <div>
+          <Link to="/user-images"><Button>Mes images</Button></Link>
+        </div>
+      </div>
       <h2 className="mb-5 mt-5">5 dernières images !</h2>
       <ImageGallery images={userImages} refresh={handleRefresh} sortByDate={false} />
       <div className="mb-5" />
