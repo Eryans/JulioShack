7587591f1b64f3b15b8ce0c32f5ac32a5964e131
Julio Punkito <julesnoir@hotmail.fr>
2023-06-02 17:57:20 +0200

    refactor(client): :speech_balloon: translated text to french and refactored homepage

---
+++ client/src/components/HomeText/index.jsx
@@ -0,0 +1,59 @@
+const HomeText = () => {
+  return (
+    <div>
+      <h1 className="text-center">
+        Bienvenue dans notre monde de la folie de l'hébergement
+        d'images !
+      </h1>
+      <br />
+      Félicitations pour avoir réussi à entrer dans notre royaume chaotique où
+      nous accueillons l'art du shitposting à bras ouverts. Nous sommes là pour
+      vous fournir la meilleure application d'images qui répond à vos besoins
+      les plus fous et les plus absurdes.
+      <br />
+      Maintenant que vous êtes connecté, laissez-nous vous guider à travers
+      cette folie avec une visite rapide des merveilles de navigation qui vous
+      attendent :
+      <br />
+      <br />
+      <ul>
+        <li>
+          1. "Voir mes images" : Ce bouton magique vous transportera vers un
+          royaume où réside votre collection personnelle d'absurdités.
+          Contemplez le chef-d'œuvre de vos propres créations, le coffre au
+          trésor des mèmes et des photos douteuses. Explorez et redécouvrez le
+          génie tordu caché au sein de votre galerie d'images.
+        </li>
+        <li>
+          2. "Parcourir les images publiques" : Aventurez-vous dans l'immensité
+          des images publiques créées par d'autres aventuriers comme vous.
+          Préparez-vous à être émerveillé, déconcerté et peut-être traumatisé à
+          vie par les créations étranges et merveilleuses partagées par notre
+          communauté éclectique.
+        </li>
+        <li>
+          3. Et ici, en ce moment même, sur cette page même, contemplez la
+          puissance de "Télécharger une image" ! Laissez libre cours à votre
+          imagination et offrez au monde votre perspective unique. Qu'il
+          s'agisse d'une photo d'un chat avec une moustache ou d'un kaléidoscope
+          hypnotisant de couleurs, le choix vous appartient. Laissez libre cours
+          à votre imagination et laissez le monde s'émerveiller !
+        </li>
+      </ul>
+      Alors attachez-vous et profitez du manège tumultueux de notre paradis de
+      l'hébergement d'images. Embrassez l'absurdité, embrassez la folie et
+      n'hésitez jamais à libérer le seigneur du meme qui sommeille en vous.
+      Rappelez-vous, dans ce royaume, la normalité n'est qu'un lointain
+      souvenir.
+      <br />
+      Bonne exploration, cher aventurier ! Et souvenez-vous toujours, plus c'est
+      étrange, mieux c'est !
+      <br />
+      P.S. Restez à l'écoute pour notre prochaine fonctionnalité qui vous
+      permettra de transformer vos images en GIFs qui feront douter le monde de
+      son existence même. Ça va être époustouflant !
+    </div>
+  )
+}
+
+export default HomeText
--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -1,95 +1,31 @@
-import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-
-import { AuthState } from '../../context/AuthProvider'
-import { Notify } from '../../utils'
+import { useEffect, useState } from 'react'
 import Wrapper from '../../components/Wrapper'
+import { getPublicImages } from '../../actions/imageAction'
+import ImageGallery from '../../components/ImageGallery'
+import HomeText from '../../components/HomeText'
 
 const HomePage = () => {
-  const [privateMessage, setPrivateMessage] = useState('')
+  const [userImages, setUserImages] = useState([])
+  const [refresh, setRefresh] = useState(false)
 
-  const navigate = useNavigate()
-  const { auth } = AuthState()
-
-  const fetchPrivateDate = async () => {
-    try {
-      const response = await fetch('/api/private', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${auth.token}`,
-        },
-      })
-      const data = await response.json()
-
-      if (data.success) {
-        setPrivateMessage(data.data)
-        return Notify(data.data, 'success')
-      } else {
-        navigate('/login')
-        return Notify('You are not authorized please login', 'error')
-      }
-    } catch (error) {
-      localStorage.removeItem('auth')
-      navigate('/login')
-      return Notify('Internal server error', 'error')
-    }
+  const handleRefresh = () => {
+    setRefresh(!refresh)
   }
-
   useEffect(() => {
-    fetchPrivateDate()
-    // eslint-disable-next-line
-  }, [])
+    const fetchImage = async () => {
+      const images = await getPublicImages(5)
+      if (images) setUserImages(images.data)
+    }
+    fetchImage()
+  }, [refresh])
 
   return (
     <Wrapper>
-      <div>
-        Welcome to our wacky world of image hosting madness!
-        <br />
-        Congratulations on successfully entering our chaotic realm where we
-        embrace the art of shitposting with open arms. We're here to provide you
-        with the best image app that caters to your wildest and most nonsensical
-        needs.
-        <br />
-        Now that you're logged in, let us guide you through this madness with a
-        quick tour of the various navigational wonders that await you:
-        <br />
-        <br />
-        <ul>
-        <li>
-          1. "See My Images": This magical button will transport you to a realm
-          where your personal collection of absurdity resides. Behold the
-          masterpiece of your own creations, the treasure trove of memes and
-          questionable photos. Explore and rediscover the twisted genius hidden
-          within your own image gallery.
-        </li>
-        <li>
-          2. "Browse Public Images": Venture into the vast expanse of public
-          images created by fellow adventurers like yourself. Prepare to be
-          amazed, bewildered, and possibly scarred for life by the bizarre and
-          wonderful creations shared by our eclectic community.
-        </li>
-        <li>
-          3. And right here, right now, on this very page, behold the power of
-          "Upload an Image"! Unleash your creative juices and gift the world with
-          your unique perspective. Whether it's a picture of a cat with a mustache
-          or a mesmerizing kaleidoscope of colors, the choice is yours. Let your
-          imagination run wild, and let the world gasp in awe!
-        </li>
-        </ul>
-        So, buckle up and enjoy the rollercoaster ride that is our image hosting
-        paradise. Embrace the absurdity, embrace the madness, and never hesitate
-        to unleash your inner meme lord. Remember, in this realm, normality is
-        nothing but a distant memory.
-        <br />
-        Happy exploring, fellow adventurer! And always remember, the weirder,
-        the better!
-        <br />
-        P.S. Stay tuned for our upcoming feature that allows you to turn your
-        images into GIFs that will make the world question its very existence.
-        It's going to be mind-blowing!
-      </div>
-      <span>{privateMessage}</span>
+      <HomeText />
+
+      <h2 className="mb-5 mt-5">5 dernières images !</h2>
+      <ImageGallery images={userImages} refresh={handleRefresh} />
+      <div className="mb-5" />
     </Wrapper>
   )
 }
