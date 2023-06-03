ca48416c37fd6f1d7de81dc483f12fa58448d2aa
Julio Punkito <julesnoir@hotmail.fr>
2023-06-03 11:47:30 +0200

    refactor(client): :recycle: multerInput image privacy input is now optionnal, also update css

--- client/src/components/MulterInput/index.jsx
+++ client/src/components/MulterInput/index.jsx
@@ -4,25 +4,25 @@ import { uploadUserImage } from '../../actions/imageAction'
 import { AuthState } from '../../context/AuthProvider'
 import { Notify } from '../../utils'
 
-const MulterInput = ({ refresh }) => {
+const MulterInput = ({ refresh, canSetPrivacy = true }) => {
   const [selectedFile, setSelectedFile] = useState(null)
-  const [isPrivate, setIsPrivate] = useState(false);
+  const [isPrivate, setIsPrivate] = useState(false)
 
   const { auth } = AuthState()
-  
+
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0])
   }
 
   const handleCheckboxChange = (event) => {
-    setIsPrivate(event.target.checked);
-  };
+    setIsPrivate(event.target.checked)
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault()
     const formData = new FormData()
     formData.append('image', selectedFile)
-    formData.append('isPrivate',isPrivate)
+    formData.append('isPrivate', isPrivate)
     // Envoyer formData vers votre API pour gérer l'upload avec Multer
     // Exemple avec fetch :
     uploadUserImage(auth._id, formData)
@@ -30,7 +30,7 @@ const MulterInput = ({ refresh }) => {
         formData.delete('image')
         setSelectedFile(null)
         refresh()
-        return Notify('Wouhou ! Image uploaday !','success')
+        return Notify('Wouhou ! Image uploaday !', 'success')
         // Faire quelque chose avec la réponse du serveur
       })
       .catch((error) => {
@@ -41,19 +41,21 @@ const MulterInput = ({ refresh }) => {
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit} className='d-flex flex-column gap-3'>
       <Form.Group controlId="fileUpload">
         <Form.Label>Choisir un fichier</Form.Label>
         <Form.Control type="file" onChange={handleFileChange} />
       </Form.Group>
-      <Form.Group controlId="isPrivateCheckbox">
-        <Form.Check
-          type="checkbox"
-          label="Fichier privé"
-          checked={isPrivate}
-          onChange={handleCheckboxChange}
-        />
-      </Form.Group>
+      {canSetPrivacy && (
+        <Form.Group controlId="isPrivateCheckbox">
+          <Form.Check
+            type="checkbox"
+            label="Fichier privé"
+            checked={isPrivate}
+            onChange={handleCheckboxChange}
+          />
+        </Form.Group>
+      )}
       <Button type="submit">Envoyer</Button>
     </Form>
   )
