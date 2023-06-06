2870f3bbbeae3e32889ae5ce6423956134b4add0
Julio Punkito <julesnoir@hotmail.fr>
2023-06-06 11:38:12 +0200

    feat(client): :sparkles: added preview to multerInput

--- client/src/components/MulterInput/index.jsx
+++ client/src/components/MulterInput/index.jsx
@@ -1,65 +1,102 @@
-import React, { useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
-import { uploadUserImage } from '../../actions/imageAction'
-import { AuthState } from '../../context/AuthProvider'
-import { Notify } from '../../utils'
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { uploadUserImage } from "../../actions/imageAction";
+import { AuthState } from "../../context/AuthProvider";
+import { Notify } from "../../utils";
 import { FileUploader } from "react-drag-drop-files";
-import './multerinput.css'
-const MulterInput = ({ refresh, canSetPrivacy = true , inputLabel="Choissisez ou glisser une image ici.  "}) => {
-  const [selectedFile, setSelectedFile] = useState(null)
-  const [isPrivate, setIsPrivate] = useState(false)
-  const fileTypes = ["JPG", "PNG", "GIF"];
-  const { auth } = AuthState()
+import "./multerinput.css";
+const MulterInput = ({
+	refresh,
+	canSetPrivacy = true,
+	inputLabel = "Choissisez ou glisser une image ici.  ",
+}) => {
+	const [selectedFile, setSelectedFile] = useState(null);
+	const [isPrivate, setIsPrivate] = useState(false);
+	const fileTypes = ["JPG", "PNG", "GIF"];
+	const { auth } = AuthState();
+	const [previewImage, setPreviewImage] = useState("");
 
-  const handleFileChange = (file) => {
-    setSelectedFile(file)
-  }
+	const handleFileChange = (file) => {
+		setSelectedFile(file);
+		const reader = new FileReader();
 
-  const handleCheckboxChange = (event) => {
-    setIsPrivate(event.target.checked)
-  }
+		reader.onloadend = () => {
+			setPreviewImage(reader.result);
+		};
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    const formData = new FormData()
-    formData.append('image', selectedFile)
-    formData.append('isPrivate', isPrivate)
-    // Envoyer formData vers votre API pour gérer l'upload avec Multer
-    // Exemple avec fetch :
-    uploadUserImage(auth._id, formData)
-      .then((data) => {
-        formData.delete('image')
-        setSelectedFile(null)
-        refresh()
-        return Notify('Wouhou ! Image uploaday !', 'success')
-        // Faire quelque chose avec la réponse du serveur
-      })
-      .catch((error) => {
-        console.error(error)
-        return Notify(error, 'warn')
-        // Gérer les erreurs
-      })
-  }
+		if (file) {
+			reader.readAsDataURL(file);
+		}
+		console.log(file);
+	};
 
-  return (
-    <Form onSubmit={handleSubmit} className='d-flex flex-column gap-3' style={{maxWidth:'322px'}}>
-      <Form.Group controlId="fileUpload">
-        <FileUploader classes="uploader" handleChange={handleFileChange} name="file" types={fileTypes} label={inputLabel}/>
-        
-      </Form.Group>
-      {canSetPrivacy && (
-        <Form.Group controlId="isPrivateCheckbox">
-          <Form.Check
-            type="checkbox"
-            label="Fichier privé"
-            checked={isPrivate}
-            onChange={handleCheckboxChange}
-          />
-        </Form.Group>
-      )}
-      <Button className="w-50 align-self-end" type="submit">Envoyer</Button>
-    </Form>
-  )
-}
+	const handleCheckboxChange = (event) => {
+		setIsPrivate(event.target.checked);
+	};
 
-export default MulterInput
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const formData = new FormData();
+		formData.append("isPrivate", isPrivate);
+		formData.append("image", selectedFile);
+		setPreviewImage(null);
+		// Envoyer formData vers votre API pour gérer l'upload avec Multer
+		// Exemple avec fetch :
+		uploadUserImage(auth._id, formData)
+			.then((data) => {
+				formData.delete("image");
+				setSelectedFile(null);
+				refresh();
+				return Notify("Wouhou ! Image uploaday !", "success");
+				// Faire quelque chose avec la réponse du serveur
+			})
+			.catch((error) => {
+				console.error(error);
+				return Notify(error, "warn");
+				// Gérer les erreurs
+			});
+	};
+
+	return (
+		<Form
+			onSubmit={handleSubmit}
+			className="d-flex flex-column gap-3"
+			style={{ maxWidth: "322px" }}
+		>
+			{previewImage && (
+				<>
+					Prévisualisation :
+					<img
+						src={previewImage}
+						alt=""
+						style={{ maxWidth: "100%", marginTop: "10px" }}
+					/>
+				</>
+			)}
+			<Form.Group controlId="fileUpload">
+				<FileUploader
+					classes="uploader"
+					handleChange={handleFileChange}
+					name="file"
+					types={fileTypes}
+					label={inputLabel}
+				/>
+			</Form.Group>
+			{canSetPrivacy && (
+				<Form.Group controlId="isPrivateCheckbox">
+					<Form.Check
+						type="checkbox"
+						label="Fichier privé"
+						checked={isPrivate}
+						onChange={handleCheckboxChange}
+					/>
+				</Form.Group>
+			)}
+			<Button className="w-50 align-self-end" type="submit">
+				Envoyer
+			</Button>
+		</Form>
+	);
+};
+
+export default MulterInput;
