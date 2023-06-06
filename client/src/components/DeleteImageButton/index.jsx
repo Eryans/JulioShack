104c4e31e6ec9449eec82f7401eeccef7af43667
Julio Punkito <julesnoir@hotmail.fr>
2023-06-06 14:45:57 +0200

    style(client): :lipstick: added dark theme color to delete modal

--- client/src/components/DeleteImageButton/index.jsx
+++ client/src/components/DeleteImageButton/index.jsx
@@ -1,58 +1,71 @@
-import React, { useState } from 'react'
-import { Button, Image, Modal } from 'react-bootstrap'
-import { deleteUserImage } from '../../actions/imageAction'
-import { Notify } from '../../utils'
+import React, { useState } from "react";
+import { Button, Image, Modal } from "react-bootstrap";
+import { deleteUserImage } from "../../actions/imageAction";
+import { Notify } from "../../utils";
 
 const DeleteImageButton = ({ imageId, refresh, handleCloseModalOrigin }) => {
-  const [showModal, setShowModal] = useState(false)
+	const [showModal, setShowModal] = useState(false);
 
-  const handleDeleteImage = () => {
-    deleteUserImage(imageId)
-      .then(() => {
-        setShowModal(false)
-        handleCloseModalOrigin()
-        refresh()
-        return Notify('Image supprimay :<', 'success')
-      })
-      .catch((error) => {
-        // Gestion des erreurs
-        console.error(error)
-        return Notify(`Shit happend ! ${error}`, 'warn')
-      })
-  }
+	const handleDeleteImage = () => {
+		deleteUserImage(imageId)
+			.then(() => {
+				setShowModal(false);
+				handleCloseModalOrigin();
+				refresh();
+				return Notify("Image supprimay :<", "success");
+			})
+			.catch((error) => {
+				// Gestion des erreurs
+				console.error(error);
+				return Notify(`Shit happend ! ${error}`, "warn");
+			});
+	};
 
-  const handleCloseModal = () => {
-    setShowModal(false)
-  }
+	const handleCloseModal = () => {
+		setShowModal(false);
+	};
 
-  return (
-    <>
-      <Button variant="danger" onClick={() => setShowModal(true)}>
-        Supprimer l'image
-      </Button>
+	return (
+		<>
+			<Button variant="danger" onClick={() => setShowModal(true)}>
+				Supprimer l'image
+			</Button>
 
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Confirmation de suppression</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Image
-            src="https://media.tenor.com/rGrmSnyBPlMAAAAC/peepo-sad.gif"
-            className="w-100"
-          />
-          Êtes-vous sûr de vouloir supprimer cette image ?
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>
-            Annuler
-          </Button>
-          <Button variant="danger" onClick={handleDeleteImage}>
-            Supprimer
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  )
-}
+			<Modal show={showModal} onHide={handleCloseModal}>
+				<Modal.Header
+					closeButton
+					style={{
+						backgroundColor: "var(--grey-bg)",
+					}}
+				>
+					<Modal.Title>Confirmation de suppression</Modal.Title>
+				</Modal.Header>
+				<Modal.Body
+					style={{
+						backgroundColor: "var(--grey-bg)",
+					}}
+				>
+					<Image
+						src="https://media.tenor.com/rGrmSnyBPlMAAAAC/peepo-sad.gif"
+						className="w-100"
+					/>
+					Êtes-vous sûr de vouloir supprimer cette image ?
+				</Modal.Body>
+				<Modal.Footer
+					style={{
+						backgroundColor: "var(--grey-bg)",
+					}}
+				>
+					<Button variant="secondary" onClick={handleCloseModal}>
+						Annuler
+					</Button>
+					<Button variant="danger" onClick={handleDeleteImage}>
+						Supprimer
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	);
+};
 
-export default DeleteImageButton
+export default DeleteImageButton;
